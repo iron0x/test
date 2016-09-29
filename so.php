b72f84d75b8a65909c8c7aa37cb4a334554171fe
iron0x <2727586680@qq.com>
2016-09-29 09:06:03 +0800

    15ed

--- so.php
+++ so.php
@@ -1,4 +1,26 @@
 <?php
-$wsdlURL = "http://gboss.id5.cn/services/QueryValidatorServices?wsdl";
+#$wsdlURL = "http://gboss.id5.cn/services/QueryValidatorServices?wsdl";
 
-$soap = new SoapClient ( $wsdlURL );var_dump($soap);exit;
+#$soap = new SoapClient ( $wsdlURL );var_dump($soap);exit;
+
+class one
+{
+
+	const SS = 'sdddsfmdskfvd';
+#	define('SS', 'asdd');
+
+	public function __construct()
+	{
+		define('ONE', 'ddddddddddddd');
+
+		echo self::SS;
+	}
+}
+
+$a = new one;
+
+echo ONE;
+
+echo one::SS;
+
+#echo SS;
